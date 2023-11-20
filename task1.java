19579042275fd2a9ee2517fe592c28acab5ee0d5
Kira Dobrynina <116497635+kiradob@users.noreply.github.com>
2023-11-20 22:08:05 +0700

    added task1
    added task1

---
+++ task1.java
@@ -0,0 +1,13 @@
+package seminars.third;
+
+public class task1 {
+    // HW 3.1. Нужно покрыть тестами метод на 100%
+    // Метод проверяет, является ли целое число записанное в переменную n, чётным (true) либо нечётным (false).
+
+    public class Numbers {
+    
+        public boolean CheckEvenOddNumber(int number){
+            return number % 2 ==0;
+        }
+    }
+}
